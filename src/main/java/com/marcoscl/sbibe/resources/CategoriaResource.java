c9cc98c557b624ccede520a5f857f19c629080f4
Marcos Lima <marcosanthony21@hotmail.com>
2020-01-25 20:24:01 -0300

    Inserindo Categoria com POsT
    - Usado URI

--- src/main/java/com/marcoscl/sbibe/resources/CategoriaResource.java
+++ src/main/java/com/marcoscl/sbibe/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.marcoscl.sbibe.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcoscl.sbibe.domain.Categoria;
 import com.marcoscl.sbibe.services.CategoriaService;
@@ -23,6 +27,14 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 				
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@RequestBody Categoria categoria) {
+		categoria = service.inserir(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
