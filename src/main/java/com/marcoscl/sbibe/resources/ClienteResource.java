e409a25d3930b0cec638e8ca09437893c87dc1ee
Marcos Lima <marcosanthony21@hotmail.com>
2020-01-27 16:37:11 -0300

    Cliente - PUT, GET e DELETE
    - Feita condicional no construtor do Cliente - tipo
    - Usada anotacao de validacao de Email em ClienteDTO
    - Criado metodo auxiliar no ClienteService para atualizar apenas nome e meail do Cliente

--- src/main/java/com/marcoscl/sbibe/resources/ClienteResource.java
+++ src/main/java/com/marcoscl/sbibe/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.marcoscl.sbibe.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcoscl.sbibe.domain.Cliente;
+import com.marcoscl.sbibe.dto.ClienteDTO;
 import com.marcoscl.sbibe.services.ClienteService;
 
 @RestController
@@ -23,6 +32,39 @@ public class ClienteResource {
 		Cliente obj = service.buscar(id);
 				
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> editar(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Integer id){
+		Cliente cliente = service.apartirDTO(clienteDTO);
+		cliente.setId(id);
+		cliente = service.editar(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Cliente> excluir(@PathVariable Integer id) {
+		service.excluir(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> listarTudo() {
+		List<Cliente> clientes  = service.listarTudo();
+		List<ClienteDTO> clientesDTO = clientes.stream().map(
+				cliente -> new ClienteDTO(cliente)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(clientesDTO);
+	}
+	
+	@RequestMapping(value = "/pagina", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> buscarPagina(		
+			@RequestParam(value = "pagina", defaultValue = "0") Integer pagina, 
+			@RequestParam(value = "quantLinhas", defaultValue = "24") Integer quantLinhas, 
+			@RequestParam(value = "ordenacao", defaultValue = "nome") String ordenacao, 
+			@RequestParam(value = "direcao", defaultValue = "ASC") String direcao) {
+		Page<Cliente> clientes  = service.buscarPagina(pagina, quantLinhas, ordenacao, direcao);
+		Page<ClienteDTO> clientesDTO = clientes.map(cliente -> new ClienteDTO(cliente));
+		return ResponseEntity.ok().body(clientesDTO);
 	}
 	
 }
--- src/main/java/com/marcoscl/sbibe/services/ClienteService.java
+++ src/main/java/com/marcoscl/sbibe/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.marcoscl.sbibe.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcoscl.sbibe.domain.Cliente;
+import com.marcoscl.sbibe.dto.ClienteDTO;
 import com.marcoscl.sbibe.repositories.ClienteRepository;
+import com.marcoscl.sbibe.services.exceptions.DataIntegrityException;
 import com.marcoscl.sbibe.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +27,42 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 				"Objeto não encontrado!" + id + ", Tipo: " + Cliente.class.getName()
 			));
+	}
+	
+	public Cliente editar(Cliente cliente) {
+		Cliente novoCliente = buscar(cliente.getId());
+		atualizarDados(novoCliente, cliente);
+		return repo.save(novoCliente);
+	}
+
+	public void excluir(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não é possível excluir porque há entidades relacionadas");
+			}
+	}
+
+	public List<Cliente> listarTudo() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> buscarPagina(Integer pagina, Integer quantLinhas, String ordenacao, String direcao) {
+		PageRequest pageRequest = PageRequest.of(pagina, quantLinhas, Direction.valueOf(direcao), ordenacao);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente apartirDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+//	auxiliar a atualizar/editar os dados novo
+	private void atualizarDados(Cliente novoCliente, Cliente cliente) {
+		novoCliente.setNome(cliente.getNome());
+		novoCliente.setEmail(cliente.getEmail());
 	}
 	
 }
