8e364f42f362fbb40436a98e8fe6c6de6140c6a2
Marcos Lima <marcosanthony21@hotmail.com>
2020-01-28 15:19:16 -0300

    Validacao customizada: email nao repetido ao inserir Cliente
    - Marcada coluna email como registro unico na tabela
    - ClienteRepository usado recurso do Spring Data usado metodo findByEmail no qual o framework automaticamente vai detectar que quer fazer uma busca por email e vai implmentar o metodo

--- src/main/java/com/marcoscl/sbibe/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcoscl/sbibe/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marcoscl.sbibe.domain.Cliente;
 import com.marcoscl.sbibe.domain.enums.TipoCliente;
 import com.marcoscl.sbibe.dto.ClienteNovoDTO;
+import com.marcoscl.sbibe.repositories.ClienteRepository;
 import com.marcoscl.sbibe.resources.exceptions.CampoMensagem;
 import com.marcoscl.sbibe.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNovoDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) 
 				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			lista.add(new CampoMensagem("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			lista.add(new CampoMensagem("email", "Email já existente"));
 		}
 		
 // inclua os testes aqui, inserindo erros na lista
