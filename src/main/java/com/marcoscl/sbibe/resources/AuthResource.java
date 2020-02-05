8897d3040d6118c41128ad4ca74b7ce709c2f9e2
Marcos Lima <marcosanthony21@hotmail.com>
2020-02-05 01:14:19 -0300

    Atualizar token

---
+++ src/main/java/com/marcoscl/sbibe/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.marcoscl.sbibe.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcoscl.sbibe.security.JWTUtil;
+import com.marcoscl.sbibe.security.UsuarioSpringSecurity;
+import com.marcoscl.sbibe.services.UsuarioService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/atualizar_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> atualizarToken(HttpServletResponse resposta) {
+		UsuarioSpringSecurity usuario = UsuarioService.autenticado();
+		String token = jwtUtil.gerarToken(usuario.getUsername());
+		resposta.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
